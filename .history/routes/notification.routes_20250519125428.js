420e9990a1744d6d11e2b2af2bd1c3f893f49164
teja464 <tnukavarapu@gmail.com>
2025-05-19 16:03:16 +0530

    render commit

--- .history/routes/notification.routes_20250519125428.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userAuth = require('../middlewares/userAuth');
-const { getNotifications, clearNotifications } = require('../controllers/notification.controller');
-
-router.get('/notification', userAuth, getNotifications);  // Fetch notifications
-router.delete('/notification', userAuth, clearNotifications);  // Clear all notifications
-
-module.exports = router;
